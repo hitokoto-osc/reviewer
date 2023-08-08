e81b3cb683ec7095c746866860aa62bad9314f0a
Jonson Petard <41122242+greenhat616@users.noreply.github.com>
2023-08-08 13:56:13 +0800

    feat(controller, poll): create new poll

--- internal/controller/poll/poll_v1_get_poll_marks.go
+++ internal/controller/poll/poll_v1_get_poll_marks.go
@@ -3,6 +3,11 @@ package poll
 import (
 	"context"
 
+	"github.com/hitokoto-osc/reviewer/internal/consts"
+	"github.com/hitokoto-osc/reviewer/internal/model"
+	"github.com/hitokoto-osc/reviewer/internal/service"
+	"github.com/hitokoto-osc/reviewer/utility/time"
+
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
 
@@ -10,5 +15,21 @@ import (
 )
 
 func (c *ControllerV1) GetPollMarks(ctx context.Context, req *v1.GetPollMarksReq) (res *v1.GetPollMarksRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	marks, err := service.Poll().GetPollMarkLabels(ctx)
+	if err != nil {
+		return nil, gerror.WrapCode(gcode.CodeOperationFailed, err, "获取投票标签失败")
+	}
+	pollMarks := make([]model.PollMark, len(marks))
+	for i, mark := range marks {
+		pollMarks[i] = model.PollMark{
+			ID:        mark.Id,
+			Text:      mark.Text,
+			Level:     consts.PollMarkLevel(mark.Level),
+			Property:  consts.PollMarkProperty(mark.Property),
+			UpdatedAt: (*time.Time)(mark.UpdatedAt),
+			CreatedAt: (*time.Time)(mark.CreatedAt),
+		}
+	}
+	r := v1.GetPollMarksRes(pollMarks)
+	return &r, nil
 }
